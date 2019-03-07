d4f0ddfc51fce52d9c71418eafd26c1b1edd5765
B S Lakshmi <b.s.lakshmi@ericsson.com>
2019-03-07 16:32:19 +0530

    Added new class under main

---
+++ src/main/java/com/main/test/SampleMainTestClass.java
@@ -0,0 +1,12 @@
+package com.main.test;
+
+import org.testng.annotations.Test;
+
+public class SampleMainTestClass {
+	
+	@Test
+	public void mainTest(){
+		System.out.println("Inside main test");
+	}
+
+}
